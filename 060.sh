710a48e190b498f8a006b2cb3bc503f561f0c263
Edy Epumuceno Rodrigues Junior <edyjrdev@gmail.com>
2025-10-09 10:21:29 -0300

    60

--- 060.sh
+++ 060.sh
@@ -2,4 +2,23 @@
 
 # Autor: Edy Junior
 
-# Objetivo
+# Objetivo Formatar texto com awk
+
+
+#Criar Arquivo de Entrada
+touch arquivo.csv
+echo "Nome;Idade;Sexo" > arquivo.csv
+echo "Edy;47;Masculino" >> arquivo.csv
+echo "Laila;38;Feminino" >> arquivo.csv
+echo "Sara;4;Feminino" >> arquivo.csv
+
+echo "Original"
+cat arquivo.csv
+
+
+#Pegar Colunas com awd
+echo "Colunas 1 e 3"
+awk -F';' '{print$1, $3}' arquivo.csv
+
+# soma das idades
+awk -F';' '{total += $2} END {print "Vivencias:", total}' arquivo.csv
